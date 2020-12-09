92baa6682dbf0757961999b17c06092610d75c90
gelopez21 <gelopez1984@gmail.com>
2020-12-09 01:10:28 -0600

    working on

--- static/gtrends.js
+++ static/gtrends.js
@@ -1,3 +1,31 @@
+(function() {
+
+  var header    = document.querySelector('h1');
+  // var selectBox = document.querySelector('select');
+  // var runButton = document.querySelector('button');
+
+  // On SelectBox Change
+  // selectBox.addEventListener('change', function() {
+  //   triggerAnimation();
+  // })
+
+  // // On "run animation" button clicked
+  // runButton.addEventListener('click', function(e) {
+  //   e.preventDefault();
+  //   triggerAnimation();
+  // })
+
+  function triggerAnimation() {
+
+    var animation = selectBox.value;
+    header.className = "";
+    setTimeout(function() {
+      header.className = "animated " + animation;
+    }, 10);
+
+  }
+
+})();
 
 // Make a dropdown Button
 
