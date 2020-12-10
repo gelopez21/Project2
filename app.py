17454f532f6f6929136ae3a14616bb2444f53a2c
gelopez21 <gelopez1984@gmail.com>
2020-12-09 23:00:08 -0600

    Working on it

--- app.py
+++ app.py
@@ -13,23 +13,15 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# db = googleData_db
-# collection = db.google_collection
+
 client = pymongo.MongoClient()
 mongo = PyMongo(app, uri="mongodb://gelopez21:Hershey*963.@localhost:27017/googleData_db?ssl=true&authSource=admin&replicaSet=atlas-96tmk6-shard-0")
 
 
 @app.route("/")
 def home():
-    # google_collection = mongo.db.google_collection.find_one()
     return render_template("index.html")
-# @app.route('/scrape')
-# def scrape():
-#     mars_results = ??.scrape()
-#     mars_collection = mongo.db.mars_collection
-    
-#     mars_collection.update({},mars_results, upsert=True)
-#     return redirect("/", code=302)
+
 
 
 if __name__ == "__main__":
