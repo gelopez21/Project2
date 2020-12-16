7a32fda1afabf87133edc11502f3e94c9df1fed6
gelopez21 <gelopez1984@gmail.com>
2020-12-16 01:29:30 -0600

    reloaded

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+# "0903822487659f81a0471ca69b5eed36"
 API_KEY = os.environ.get('API_KEY') or '0903822487659f81a0471ca69b5eed36'
 
 # MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/stops.collection_stops'
--- static/js/config.js
+++ static/js/config.js
@@ -4,4 +4,4 @@ eeGeo_api_key = "0903822487659f81a0471ca69b5eed36"
 
 
 // pk.eyJ1IjoiamVyZW15bW9za293aXR6IiwiYSI6ImNrM2JveTF2eTBubGYzaXFsNmU0dmtybHoifQ.ijOeYOD-FrnF78IYvD369w
-mapBox_api_key = "pk.eyJ1IjoiamVyZW15bW9za293aXR6IiwiYSI6ImNrM2JveTF2eTBubGYzaXFsNmU0dmtybHoifQ.ijOeYOD-FrnF78IYvD369w"
+mapBox_api_key = "pk.eyJ1IjoiZXJlc2RpdmluYSIsImEiOiJja2hwa2w4OWMwMHVsMnJvOGE3ZDIxdHlwIn0.PfbFm_UP5Rw2C66p0x9Eng"
